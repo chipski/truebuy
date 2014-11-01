0568ab471889d92c6c466f65199026a76c98d6c6
Chip Vanek <chipski@mac.com>
2014-11-01 13:11:50 -0700

    problem with goodzer

--- app/mixins/goodzer.rb
+++ app/mixins/goodzer.rb
@@ -1,4 +1,4 @@
-class Goodzer < ActiveResource::Base
+class Goodzer 
   self.site = "http://api.goodzer.com/products/v0.1/search_locations/?apiKey=7abbdf2e073c361c22f319bb8d545f4f&lat=37.381168365478516&lng=-122.08232879638672&query=strollers"
   self.element_name = "product"
   
