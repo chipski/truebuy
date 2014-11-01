f34c28f49f8c5c0541360fb12944b018c5da0565
Chip Vanek <chipski@mac.com>
2014-11-01 13:14:07 -0700

    problem with goodzer

--- app/mixins/goodzer.rb
+++ app/mixins/goodzer.rb
@@ -1,7 +1,10 @@
 class Goodzer 
-  self.site = "http://api.goodzer.com/products/v0.1/search_locations/?apiKey=7abbdf2e073c361c22f319bb8d545f4f&lat=37.381168365478516&lng=-122.08232879638672&query=strollers"
-  self.element_name = "product"
   
+  def inititalize
+    self.site = "http://api.goodzer.com/products/v0.1/search_locations/?apiKey=7abbdf2e073c361c22f319bb8d545f4f&lat=37.381168365478516&lng=-122.08232879638672&query=strollers"
+    self.element_name = "product"
+  end
+
   def stroller
     @def = "%lat=37.381168365478516&lng=-122.08232879638672%query=strollers"
     
